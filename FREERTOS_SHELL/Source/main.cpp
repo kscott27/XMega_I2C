55b11114819b8e3991363069a723f717018976ae
kscott27 <kmscott27@gmail.com>
2018-11-22 00:23:48 -0800

    Uncommented instantiation of Sensor class in main() and it still compiles!

--- FREERTOS_SHELL/Source/main.cpp
+++ FREERTOS_SHELL/Source/main.cpp
@@ -148,7 +148,7 @@ int main (void)
 
 	// I2CAgent i2cAgent();
 	
-	// MB1202 mb1202(&i2c);
+	MB1202 mb1202(&i2c);
 	
 	
 	//hi = i2c.is_ready(85);
@@ -157,7 +157,7 @@ int main (void)
 	// but it is desired to exercise the RTOS more thoroughly in this test program
 	new task_user ("UserInt", task_priority (0), 128, &ser_dev);
 	
-	// new task_sonar ("Sonar", task_priority (2), 128, &ser_dev, &mb1202);
+	new task_sonar ("Sonar", task_priority (2), 128, &ser_dev, &mb1202);
 	
 	// Enable high level interrupts and global interrupts
 	PMIC_CTRL = (1 << PMIC_HILVLEN_bp | 1 << PMIC_MEDLVLEN_bp | 1 << PMIC_LOLVLEN_bp);
